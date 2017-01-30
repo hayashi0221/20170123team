eface031d429ed5324e3bca2bf69f7228bafe80d
hayashi0221 <pennginnfire@yahoo.co.jp>
2017-01-30 15:57:51 +0900

    Add files via upload

---
+++ 20170130クライアント/test.php
@@ -0,0 +1,70 @@
+﻿<html>
+
+<head>
+	
+<title>test</title>
+
+</head>
+
+<body>
+
+
+<form action="jankentest.php" method = “POST”>
+
+	
+<?php
+	
+	
+$username = $_POST['te_post'];
+
+	
+if($username == null)
+
+	{
+ 
+		echo $username;
+
+	}
+	
+else if($username != null)
+	{
+
+    	echo $username;
+
+    }
+
+	
+?>
+
+	
+<br>
+
+	
+<input type="text" name="syouhai"></p>
+	
+<span style="position:relative;left:100px;top:0px">
+	
+<input type="submit" value="送信"></span>
+
+</form>
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
